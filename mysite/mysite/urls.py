3ee3587a7386e54f10161c8de65f81cd96879fb6
pbastani <retina@Helens-MacBook-Pro.local>
2014-12-19 01:27:05 -0800

    Added Avalon admin template as the base template.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,8 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = patterns('',
                        url(r'^portfolio/', include('portfolio.urls', namespace="portfolio", app_name="portfolio")),
-                       url(r'^timeline/', include('timeline.urls', namespace="timeline")),
-                       url(r'^polls/', include('polls.urls', namespace="polls")),
-                       url(r'^picit/', include('picit.urls', namespace="picit")),
+                       url(r'^listings/', include('listings.urls', namespace="listings")),
                        url(r'^admin/', include(admin.site.urls)),
                        ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
