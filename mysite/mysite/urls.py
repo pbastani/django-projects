0e341234bfd2bd46d3fde39c616aa6a38f9b4425
pbastani <retina@Helens-MacBook-Pro.local>
2014-12-21 16:51:30 -0800

    Added signing in/out/up to listings.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-                       url(r'^portfolio/', include('portfolio.urls', namespace="portfolio", app_name="portfolio")),
                        url(r'^listings/', include('listings.urls', namespace="listings")),
                        url(r'^admin/', include(admin.site.urls)),
                        ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
